c378a43368b4712931ca4880e1bf7f099f889870
Ignacio Garcia P <igarciap@fi.uba.ar>
2026-07-01 23:30:38 -0300

    saco ejemplo de pdf viewer

--- pdf-forms/src/download.ts
+++
@@ -1,8 +0,0 @@
-export function downloadFile(url: string, filename: string) {
-  const a = document.createElement("a");
-  a.href = encodeURI(url);
-  a.download = filename;
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-}
--- pdf-forms/vitest.config.ts
+++
@@ -1,3 +0,0 @@
-import { createVitestConfig } from "@repo/testing/vitest-config";
-
-export default createVitestConfig();
